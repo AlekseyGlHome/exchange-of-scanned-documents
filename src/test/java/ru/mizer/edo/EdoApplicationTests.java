92734d2671b8a9e294680505055c7f9b8354a1f5
AlekseyGl <glushenko78@mail.ru>
2022-03-21 11:36:13 +0300

    init

---
+++ src/test/java/ru/mizer/edo/EdoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mizer.edo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
