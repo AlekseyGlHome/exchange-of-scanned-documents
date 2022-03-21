9f6c600631ff4e1110080a5ecbc3dbeeaf7f6bc2
AlekseyGl <glushenko78@mail.ru>
2022-03-21 16:19:21 +0300

    add migrate db and entity classes

---
+++ src/main/java/ru/mizer/edo/model/entity/Document.java
@@ -0,0 +1,40 @@
+package ru.mizer.edo.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "documents")
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "doc_html")
+    private String docHtml;
+
+    @Column(name = "doc_path", length = 250)
+    private String docPath;
+
+    @Column(name = "header", nullable = false, length = 250)
+    private String header;
+
+    @Column(name = "is_done", nullable = false)
+    private Boolean isDone = false;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "autor_id", nullable = false)
+    private User autor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id_last_change")
+    private User userLastChange;
+
+}
---
+++ src/main/java/ru/mizer/edo/model/entity/User.java
@@ -0,0 +1,29 @@
+package ru.mizer.edo.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 250)
+    private String name;
+
+    @Column(name = "password", nullable = false, length = 250)
+    private String password;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = false;
+
+}
